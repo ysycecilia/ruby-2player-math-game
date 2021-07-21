038c8ef597e3e03826f2c7b338f2d72dac8a31f1
ysycecilia <ysycecilia@gmail.com>
2021-07-20 22:37:22 -0400

    remove method

--- GamePlayer.rb
+++ GamePlayer.rb
@@ -1,14 +1,10 @@
+# start with 3 lives, lose 1 when give wrong answer
 class GamePlayer
-  attr_accessor :lives :name
+  attr_accessor :lives, :name
   def initialize(name)
     @lives = 3
     @name = name
   end
-
-  def wrongAnswer
-    @lives -= 1
-  end
-
 
 end
 
